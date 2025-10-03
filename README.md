1b7e6041a327cce8777eca2fe1b994b4e11f92c9
Rirurin <rirurin@rirurin.com>
2025-10-03 18:06:12 +0800

    Update build script, first attempt at getting CI working

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # cri-adx
-TODO: A description
+
+A Reloaded-II + Rust library for calling methods from Criware's ADX Library statically linked to the target executable. 
+Static linking is achieved by scanning the executable for signatures that match target functions within ADX's API. 
+These signatures are either consumed from Ryo Framework or are stored within the mod.
+
+This is currently only tested with [Metaphor: ReFantazio](https://store.steampowered.com/app/2679460/Metaphor_ReFantazio/) since that's the game that I personally use this for.
+
+## Usage
+
+**[TODO]**
+
+## Building
+
+### Requirements
+
+- **Rust Nightly**. This can be installed by downloading the [Rust toolchain](https://www.rust-lang.org/tools/install), then entering `rustup toolchain install nightly` in your terminal.
+- [**.NET 9.0 SDK**](https://dotnet.microsoft.com/en-us/download/dotnet/9.0).
+- [**Powershell**](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell). Used for the build script. Note that on Windows, you'll need to run `Set-Execution-Policy Unrestricted` in Powershell if you haven't set that already to allow the build script to execute.
+
+### Building
+
+Run `./Build` to build the mod and copy it into your Reloaded-II mods folder.
+
+## Contributions
+
+**[TODO]**
+
+## Credits and Licenses
+
+**[TODO]**
---
+++ buildscript/riri.criadx.BuildScript/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace riri.criadx.BuildScript;
+
+public class SkipGlobals : Argument
+{
+    public override void HandleParams(string[] args)
+    {
+        Enabled = args[0].ToLower() switch
+        {
+            "true" => true,
+            "false" => false,
+            _ => throw new Exception($"Expected a boolean value, got {args[0]} instead")
+        };
+    }
+
+    public override int GetParamCount() => 1;
+}
+
+public class Timings : Argument
+{
+    public override void HandleParams(string[] args)
+    {
+        Enabled = args[0].ToLower() switch
+        {
+            "true" => true,
+            "false" => false,
+            _ => throw new Exception($"Expected a boolean value, got {args[0]} instead")
+        };
+    }
+
+    public override int GetParamCount() => 1;
+}
+
+public class Publish : Argument
+{
+    public override void HandleParams(string[] args)
+    {
+        Enabled = args[0].ToLower() switch
+        {
+            "true" => true,
+            "false" => false,
+            _ => throw new Exception($"Expected a boolean value, got {args[0]} instead")
+        };
+    }
+
+    public override int GetParamCount() => 1;
+}
+
+public class ArgumentList : ArgumentListBase
+{
+    public ArgumentList(string[] args) : base(args) { }
+
+    protected override Dictionary<string, Argument> SetArguments()
+    {
+        return new()
+        {
+            { "Debug", new Debug() },
+            { "SkipGlobals", new SkipGlobals() },
+            { "Timings", new Timings() },
+            { "Publish", new Publish() }
+        };
+    }
+}
+
+public class ProjectManager : ProjectManagerBase
+{
+    public override List<KeyValuePair<string, CodePackage>> GetProjects(ArgumentListBase arg, string RootPath)
+    {
+        return new List<KeyValuePair<string, CodePackage>>()
+        {
+            Register(new CSharpProject(arg, Path.Combine(RootPath, "riri.criadx"))),
+            Register(new RustCrate(arg, Path.Combine(RootPath, "cri-adx-reloaded"))),
+        };
+    }
+    public ProjectManager(ArgumentList arg, string RootPath) : base(arg, RootPath) { }
+}
+
+public class Executor : ExecutorBase<ArgumentList, ProjectManager>
+{
+    public override string BuildType
+    {
+        get => "CLIENT";
+    }
+
+    public Executor(string[] args) : base(args) { }
+
+    public override void Execute()
+    {
+        if (ArgList["Publish"].Enabled)
+        {
+            PublishState.Cleanup();
+            PublishState.GetTools();
+        }
+        PrintInformation();
+        // Create riri_hook folder if it doesn't already exist
+        Directory.CreateDirectory(Path.Combine(ProjectManager["cri-adx-reloaded"].RootPath, "riri_hook"));
+        // Build Cri ADX Bindings (Rust portion)
+        ProjectManager["cri-adx-reloaded"].Build();
+        // Build Cri ADX Bindings (C# portion)
+        if (ArgList["Publish"].Enabled)
+        {
+            ((CSharpProject)ProjectManager["riri.criadx"]).PublishBuildDirectory = PublishState.PublishBuildDirectory;
+            ((CSharpProject)ProjectManager["riri.criadx"]).TempDirectory = PublishState.TempDirectoryBuild;
+        }
+        ProjectManager["riri.criadx"].Build();
+        if (ArgList["Publish"].Enabled)
+        {
+            PublishState.CreateArtifacts();
+        }
+        else
+        {
+            // Copy output files from target folder into Reloaded mod
+            var reloadedDirectory = Path.Combine(Environment.GetEnvironmentVariable("RELOADEDIIMODS")!, "riri.criadx");
+            ((RustCrate)ProjectManager["cri-adx-reloaded"]).CopyOutputArtifacts(ArgList["Debug"].Enabled, RootPath, reloadedDirectory);
+        }
+        PrintCompleted();
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (Environment.GetEnvironmentVariable("RELOADEDIIMODS") == null)
+            throw new Exception("The environment variable RELOADEDIIMODS is not defined!");
+        var exec = new Executor(args);
+        exec.Execute();
+    }
+}
